dfe11514d696759b5156237281c51386c084fe13
yablokoff <yablokoff.tlt@gmail.com>
2013-07-29 01:56:36 +0400

    16x9 photos added.

--- js/theme.js
+++ js/theme.js
@@ -166,8 +166,9 @@ readyState(function()
         delay: 10000,
         backgrounds: [
             { src: 'img/bg1.jpg', fade: 2000 },
-            { src: 'img/bg2.jpg', fade: 2000 },
-            { src: 'img/bg3.jpg', fade: 2000 }
+            { src: 'img/bg3.jpg', fade: 2000 },
+            { src: 'img/bg4.jpg', fade: 2000 },
+            { src: 'img/bg2.jpg', fade: 2000 }
         ]
     })('overlay');
 
